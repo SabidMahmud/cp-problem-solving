ed094eb03f3e3ef22a5ac9d1e98296c87a141a91
Sabid Mahmud <sabidmahmud96@gmail.com>
2022-02-24 22:18:10 +0600

    Update sum_dif.c

--- Sum and Difference of Two Numbers/sum_dif.c
+++ Sum and Difference of Two Numbers/sum_dif.c
@@ -5,17 +5,13 @@
 
 int main()
 {
-  int a, b, m, n;
-  float x, y, p, q;
+  int a, b;
+  float x, y;
   scanf("%d %d\n", &a, &b);
   scanf("%f %f\n", &x, &y);
   if (1<=a && 1<=b && a<=10000 && b<=10000 && 1<=x && 1<=y && x<=10000 && y<=10000)
   {
-    m = a + b;
-    n = a-b;
-    p = x+y;
-    q = x-y;
-    printf("%d %d\n%.1f %.1f\n", m, n, p, q);
+   printf("%d %d\n%.1f %.1f\n", a+b, a-b, x+y, x-y);
   }else printf("Error!\n");
 
     return 0;
