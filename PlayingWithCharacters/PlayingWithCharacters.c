545ce50d57fbce23b7de70928a1f9dafd9f08f9f
Sabid Mahmud <sabidmahmud96@gmail.com>
2022-02-24 18:49:44 +0600

    Update PWChar.c

--- PlayingWithCharacters/PlayingWithCharacters.c
+++ PlayingWithCharacters/PlayingWithCharacters.c
@@ -1,17 +1,15 @@
-#include<stdio.h>
-int main()
-{
-    char ch; 
+#include <stdio.h>
+
+int main() {
+    char ch;
     char str[9];
     char sen[15];
-    
-    scanf("%c", &ch);
-    scanf("%s", str);
-    scanf("%*[^\n]%*c", sen);
-    
+    scanf("%c\n", &ch);
+    scanf("%s\n", str);
+    scanf("%*[^\n]%*c",&sen);
 
-    printf("%c\n", ch);
-    printf("%s\n", str);
-    printf("%c\n", sen);
-    getchar();
+    printf("%c\n",ch);
+    printf("%s\n",str);
+    printf("%s\n",sen);
+    return 0;
 }
