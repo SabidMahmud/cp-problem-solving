f2626e380e22405fce5a0e64b1df1b7a0fd43fec
Sabid Mahmud <sabidmahmud96@gmail.com>
2022-02-25 19:38:25 +0600

    Commit New Program

---
+++ for-loop-in-c/even-odd.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a, b;
+    int n;
+    scanf("%d\n%d", &a,&b);
+
+    for (n=a; n<=b; n++)
+    {
+        if(n>=1 && n<=9 && b<=pow(10,6))
+        {
+            if(n==1){
+                printf("one\n");
+            }
+            else if(n==2) {
+                 printf("two\n");
+            }
+            else if(n==3){
+                printf("three\n");
+            }
+            else if(n==4){
+                    printf("four\n");
+            }
+            else if(n==5){
+                printf("five\n");
+            }
+            else if(n==6){
+                printf("six\n");
+            }
+            else if(n==7){
+                printf("seven\n");
+            }
+            else if(n==8){
+                printf("eight\n");
+            }
+            else if(n==9){
+                printf("nine\n");
+            }
+        } else
+            if(n%2 == 0){
+                    printf("even\n");
+            }else
+            printf("odd\n");
+    }
+return 0;
+}
