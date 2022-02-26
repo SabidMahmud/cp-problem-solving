b62e206398da386a97266bba260719665bf64cb1
Sabid Mahmud <sabidmahmud96@gmail.com>
2022-02-26 19:37:26 +0600

    commit

--- Programming in ansi c problems/Chapter3/temperature-convertion.c
+++ Programming in ansi c problems/Chapter3/temperature-convertion.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
-//declaring symbolic constants
-#define F_LOW 0
-#define F_MAX 250
-
 int main()
 {
     //type definition
     typedef float REAL ;
     REAL fahrenheit, celsius ;
 
-    fahrenheit = F_LOW;
-     scanf("%f", &fahrenheit);
+    scanf("%f", &fahrenheit);
 
     printf("Fahrenheit Celsius\n\n");
 
         celsius = 5*(fahrenheit -32)/9;
         printf("%.1f      %.2f\n", fahrenheit, celsius);
-           // fahrenheit = fahrenheit + STEP;
 
 }
