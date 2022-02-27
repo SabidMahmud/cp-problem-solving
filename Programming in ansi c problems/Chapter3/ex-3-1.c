5a2853de73f77843d08070ed4e71cb7b1a3c1c28
Sabid Mahmud <sabidmahmud96@gmail.com>
2022-02-27 17:53:01 +0600

    update ex-3-1.c

--- Programming in ansi c problems/Chapter3/ex-3-1.c
+++ Programming in ansi c problems/Chapter3/ex-3-1.c
@@ -9,7 +9,16 @@ int main()
 
    for(i=1; i<=n; i++)
    {
-      s+=1*pow(i,-1);
+    //  s+=1*pow(i,-1); //
+    /*
+      s += 1/i;
+      here 1/i is integer/integer = integer. An int value always store the integer part of a number.
+      so if we do int/ (int) i, the value of 1/i will be an integer.
+      therefore we need to define at least one variable as float or double here.
+    */
+    s+=1.00/i; //or s+=1/(float)i;
+
+
    }
    printf("\nSum of the harmonic Series upto 1/%d terms : %.2f \n",n,s);
 }
