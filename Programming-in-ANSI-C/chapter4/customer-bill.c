208d4d49c3d9fc62813b1a3b8d1901240f9374d1
Sabid Mahmud <sabidmahmud96@gmail.com>
2022-03-06 11:41:59 +0600

    update Customer bill

---
+++ Programming-in-ANSI-C/chapter4/customer-bill.c
@@ -0,0 +1,39 @@
+/*
+    The cost of one type of mobile service is Rs. 250 plus Rs. 1.25 for each call made
+    over and avobe 100 calls. Write a program to read customer codes and calls made and print the bill for each customer.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int CustomerID, calls;
+    int CALL_MAX=100;
+    float COST_MIN=250;
+    float cost;
+
+    printf("Customer ID : ");
+        scanf("%-7d", &CustomerID);
+    printf("Total Calls Made : ");
+        scanf("%d", &calls);
+    
+    if(calls>=0 && calls<CALL_MAX)
+    {
+        printf("\n********** CUSTOMER BILL **********\n\n");
+        printf("=> Customer ID       #%d\n", CustomerID);
+        printf("=> Total Calls Made        %d \n", calls);
+        printf("------------------------------------\n");
+        printf("=> Total Cost           Rs. %.2f\n\n", COST_MIN);        
+    }
+    else
+    {
+        cost = COST_MIN+ 1.25*(calls-CALL_MAX);
+        printf("\n********** CUSTOMER BILL **********\n\n");
+        printf("=> Customer ID       #%d\n", CustomerID);
+        printf("=> Total Calls Made        %d \n", calls);
+        printf("------------------------------------\n");
+        printf("=> Total Cost          Rs. %.2f\n\n", cost);
+    }
+return 0;
+        
+}
