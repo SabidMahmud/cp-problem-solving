d3c3f41a6341bc19f7c53bbcd9e46dc94ac652f6
SabidMahmud <sabidmahmud96@gmail.com>
2022-03-10 17:32:47 +0600

    update

--- BeeCrowd/beginner/area.c
+++ BeeCrowd/beginner/area.c
@@ -4,7 +4,7 @@ int main(void)
 {
     const double pi = 3.14159;
     float A, B, C;
-    float triangulo, circulo, trapezio, quadrado, retengulo;
+    float triangulo, circulo, trapezio, quadrado, retangulo;
     scanf("%f %f %f", &A, &B, &C);
     triangulo = A*C*1/2;
     printf("TRIANGULO: %.3f\n", triangulo);
@@ -14,7 +14,7 @@ int main(void)
     printf("TRAPEZIO: %.3f\n", trapezio);
     quadrado = pow(B,2);
     printf("QUADRADO: %.3f\n", quadrado);
-    retengulo = A*B;
-    printf("RETENGULO: %.3f\n", retengulo);
+    retangulo = A*B;
+    printf("RETANGULO: %.3f\n", retangulo);
     return 0;
 }
