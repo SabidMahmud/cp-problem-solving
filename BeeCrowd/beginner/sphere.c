76851bd758b4c72a9386149387b9e2d67d5302d7
SabidMahmud <sabidmahmud96@gmail.com>
2022-03-10 13:49:37 +0600

    update sphere.c

--- BeeCrowd/beginner/sphere.c
+++ BeeCrowd/beginner/sphere.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    float R, volume;
-    const float pi = 3.14159;
-    scanf("%f", &R);
+    double R, volume;
+    const double pi = 3.14159;
+    scanf("%lf", &R);
     //volume = (4/3)*pi*R^3
-    volume = (4.0/3) * pi * R*R*R;
-    printf("VOLUME = %.3f\n", volume);
+    volume = (4.0/3) * pi * pow(R, 3);
+    printf("VOLUME = %.3lf\n", volume);
     return 0;
 }
