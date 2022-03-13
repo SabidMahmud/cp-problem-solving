3c9dfc41f865899f41bb28cf26f64bbc9e6977f9
SabidMahmud <sabidmahmud96@gmail.com>
2022-03-13 20:42:01 +0600

    Merge branch 'main' of github.com:SabidMahmud/programming-problems into main

--- BeeCrowd/beginner/interval.c
+++ BeeCrowd/beginner/interval.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 int main() {
-  float num;
-  scanf("%f", &num);
-  if (num >= 0) {
-    if (0 <= num && num <= 25) {
-      printf("Intervalo [0,25]\n");
-    } else if (25 < num && num <= 50) {
-      printf("Intervalo (25,50]\n");
-    } else if (50 < num && num <= 75) {
-      printf("Intervalo (50,75]\n");
-    } else if (75 < num && num <= 100) {
-      printf("Intervalo (75,100]\n");
-    }
+  double num;
+  scanf("%lf", &num);
+  if (0 <= num && num <= 25) {
+    printf("Intervalo [0,25]\n");
+  } else if (25 < num && num <= 50) {
+    printf("Intervalo (25,50]\n");
+  } else if (50 < num && num <= 75) {
+    printf("Intervalo (50,75]\n");
+  } else if (75 < num && num <= 100) {
+    printf("Intervalo (75,100]\n");
   } else {
-    printf("Fora de intervalo\n");
+  printf("Fora de intervalo\n");
   }
+
   return 0;
 }
