9ee3624928c97c572c02dd28be176c9369b9e389
SabidMahmud <sabidmahmud96@gmail.com>
2022-03-13 20:43:36 +0600

    add

--- BeeCrowd/beginner/interval.c
+++ BeeCrowd/beginner/interval.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 
 int main() {
-<<<<<<< HEAD
-  double num;
-  scanf("%lf", &num);
-  if (0 <= num && num <= 25) {
-    printf("Intervalo [0,25]\n");
-  } else if (25 < num && num <= 50) {
-    printf("Intervalo (25,50]\n");
-  } else if (50 < num && num <= 75) {
-    printf("Intervalo (50,75]\n");
-  } else if (75 < num && num <= 100) {
-    printf("Intervalo (75,100]\n");
-  } else {
-  printf("Fora de intervalo\n");
-  }
-
-=======
   float num;
   scanf("%f", &num);
       if (0 <= num && num <= 25) {
@@ -30,6 +14,5 @@ int main() {
     } else {
     printf("Fora de intervalo\n");
     }
->>>>>>> bb25ef2fbe6d58952c322c73379dc9b6da7fcf85
-  return 0;
+    return 0;
 }
