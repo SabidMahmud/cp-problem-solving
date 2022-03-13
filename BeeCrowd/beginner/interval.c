3f243c987055f070abdfd62c1942bbf7a0ea694b
SabidMahmud <sabidmahmud96@gmail.com>
2022-03-13 19:39:26 +0600

    update

--- BeeCrowd/beginner/interval.c
+++ BeeCrowd/beginner/interval.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
-int main()
-{
-    float number;
-    scanf("%f", &number);
-    if (number>=0) {
-        if (0<=number && number<=25)
-        {
-            printf("Intervalo [25,50]\n");
-        }
-        else if (25<number && number <=50)
-        {
-            printf("Intervalo (25,50]\n");
-        }
-        else if (75<number && number <= 100)
-        {
-            printf("Intervalo (75,100]\n");
-        }
-    }else {
-    printf("Fora de intervalo\n");
+int main(void) {
+  double num;
+  scanf("%lf", &num);
+  if (num >= 0) {
+    if (0 <= num && num <= 25) {
+      printf("Intervalo [0,25]\n");
+    } else if (25 < num && num <= 50) {
+      printf("Intervalo (25,50]\n");
+    } else if (50<num && num <=75) {
+      printf("Inrervalo (50,75]\n");
     }
-    return 0;
+    else if (75 < num && num <= 100) {
+      printf("Intervalo (75,100]\n");
+    }
+  } else {
+    printf("Fora de intervalo\n");
+  }
+  return 0;
 }
