bce18ef686e0440ac1c6f6aaccfa307d5667d08d
SabidMahmud <sabidmahmud96@gmail.com>
2022-03-17 00:38:14 +0600

    update salary increase

--- BeeCrowd/beginner/salary-increase.c
+++ BeeCrowd/beginner/salary-increase.c
@@ -11,31 +11,31 @@ int main(void) {
         s = s + i;
         printf("Novo salario: %.2f\n", s);
         printf("Reajuste ganho: %.2f\n", i);
-        printf("Em percentual: 15 %\n");
+        printf("Em percentual: 15 %%\n");
     } else if (s>=400.01 && s<=800.00) {
         i = (s*12/100);
         s = s + i;
         printf("Novo salario: %.2f\n", s);
         printf("Reajuste ganho: %.2f\n", i);
-        printf("Em percentual: 12 %\n");
+        printf("Em percentual: 12 %%\n");
     } else if (s>=800.01 && s<=1200.00) {
          i = (s*10/100);
         s = s + i;
         printf("Novo salario: %.2f\n", s);
         printf("Reajuste ganho: %.2f\n", i);
-        printf("Em percentual: 10 %\n");
+        printf("Em percentual: 10 %%\n");
     } else if (s>=1200.01 && s<=2000.00) {
          i = (s*7/100);
         s = s + i;
         printf("Novo salario: %.2f\n", s);
         printf("Reajuste ganho: %.2f\n", i);
-        printf("Em percentual: 7 %\n");
+        printf("Em percentual: 7 %%\n");
     } else if (s>2000.00) {
          i = (s*4/100);
         s = s + i;
         printf("Novo salario: %.2f\n", s);
         printf("Reajuste ganho: %.2f\n", i);
-        printf("Em percentual: 4 %\n");
+        printf("Em percentual: 4 %%\n");
     }
     return 0;
 }
