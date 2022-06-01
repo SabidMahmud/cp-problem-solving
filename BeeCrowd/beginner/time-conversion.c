923a05f2056d145ad3c109c2e27cacdc121257e5
Sabid Mahmud <sabidmahmud96@gmail.com>
2022-06-01 08:29:30 -0700

    Update time-conversion.c

--- BeeCrowd/beginner/time-conversion.c
+++ BeeCrowd/beginner/time-conversion.c
@@ -9,7 +9,7 @@ int main()
     int N;
     scanf("%d", &N);
     printf("%d:", N/HR); // printed the hours
-int rest = N%HR;         // calculate the rest of the seconds
+    int rest = N%HR;         // calculate the rest of the seconds
     printf("%d:", rest/MIN);    // print the minutes
     rest = rest%MIN;        //  calculate the rest of the seconds
     printf("%d\n", rest/SEC);   //  printed the seconds
